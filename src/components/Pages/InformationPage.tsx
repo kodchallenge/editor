26de8e8cc5f31333aa64325070035bcc4e6a64fb
Yasin Torun <yasintorun4680@gmail.com>
2022-10-15 20:55:45 +0300

    Added ReactMarkdown

--- src/components/Pages/InformationPage.tsx
+++ src/components/Pages/InformationPage.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
+const markdown = `just a link: https://kodchallenge.com`
 
 const InformationPage = () => {
   return (
-    <div>InformationPage</div>
+    <div>
+      <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />,
+    </div>
   )
 }
 
