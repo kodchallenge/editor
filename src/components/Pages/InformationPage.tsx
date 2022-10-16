8d37864201f0527509520935a529b5ba28d7ee66
Yasin Torun <yasintorun4680@gmail.com>
2022-10-16 10:55:57 +0300

    Added markdown editor.

--- src/components/Pages/InformationPage.tsx
+++ src/components/Pages/InformationPage.tsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
-
-const markdown = `just a link: https://kodchallenge.com`
+import rehype from 'rehype-highlight'
+import rehypeRaw from "rehype-raw"
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import {oneDark} from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 const InformationPage = () => {
-  return (
-    <div>
-      <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />,
+  const [markdown, setMarkdown] = useState<string | null>(null)
+
+  useEffect(() => {
+    fetch("/sample.md")
+      .then(res => res.text())
+      .then(text => {
+        setMarkdown(text)
+      })
+  }, [])
+
+  return markdown && (
+    <div className='page-tab markdown'>
+      <ReactMarkdown
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                //@ts-ignore
+                style={oneDark}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            )
+          }
+        }}
+      >
+        {markdown}
+      </ReactMarkdown>
     </div>
   )
 }
