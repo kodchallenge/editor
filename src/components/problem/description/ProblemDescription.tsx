7e313069b669fc78a1978ff76548d5b43eea23ba
Yasin Torun <yasintorun4680@gmail.com>
2022-10-30 11:26:12 +0300

    Fixed overflow of layout tab

--- src/components/problem/description/ProblemDescription.tsx
+++ src/components/problem/description/ProblemDescription.tsx
@@ -2,9 +2,13 @@ import KodLayout from 'kod-layout'
 import KodMarkdown from 'kod-markdown'
 import { css } from '@emotion/react'
 
+const tabCss = css`
+    overflow: auto !important;
+`
+
 const ProblemDescription = () => {
     return (
-        <KodLayout.Tab>
+        <KodLayout.Tab id='description-area' css={tabCss}>
             <div css={css`
                 padding: 10px;
                 margin-bottom: 50px;
