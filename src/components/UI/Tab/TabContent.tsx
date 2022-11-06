f6f542ce6c12c1def1d21959e0e0d186f3e84019
Yasin Torun <yasintorun4680@gmail.com>
2022-11-06 13:45:57 +0300

    Added TabContent ui component

--- src/components/UI/Tab/TabContent.tsx
+++ src/components/UI/Tab/TabContent.tsx
@@ -1,9 +1,37 @@
+import styled from '@emotion/styled';
 import React from 'react'
 
-const TabContent = () => {
-  return (
-    <div>TabContent</div>
-  )
+
+const Root = styled.section`
+    position: relative;
+    flex-grow: 1;
+`
+
+const Item = styled.div`
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    overflow-y: auto;
+`
+
+export type TabContentProps = React.HTMLAttributes<HTMLSelectElement> & {
+    value: number;
+    index: number;
 }
 
-export default TabContent
+export const TabContent: React.FC<TabContentProps> = ({ ...props }) => {
+    const { value, index, children, ...others } = props
+
+    if (value !== index) {
+        return <React.Fragment />
+    }
+
+    return (
+        <Root {...others}>
+            <Item>
+                {children}
+            </Item>
+        </Root>
+    )
+}
